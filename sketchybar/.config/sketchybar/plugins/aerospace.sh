fc6f73c93c552730e12b3f669c877b8b55a40a32
phurba-sherpa <phurba1404@gmail.com>
2026-07-27 21:25:42 +0545

    chore: update macos config to make it more like i3

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$AEROSPACE_FOCUSED_WORKSPACE" = "${NAME#space.}" ]; then
+sid="${NAME#space.}"
+FOCUSED=$(aerospace list-workspaces --focused)
+
+# Show if focused OR non-empty
+if [ "$sid" = "$FOCUSED" ] || aerospace list-workspaces --monitor all --empty no | grep -qx "$sid"; then
+  sketchybar --set "$NAME" drawing=on
+else
+  sketchybar --set "$NAME" drawing=off
+fi
+
+# Focus highlight
+if [ "$sid" = "$FOCUSED" ]; then
   sketchybar --set "$NAME" label.color=0xffebdbb2 background.drawing=on
 else
   sketchybar --set "$NAME" label.color=0x77ebdbb2 background.drawing=off
